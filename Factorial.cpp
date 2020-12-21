de0909717ae52793445a99942d80ae5ebafb677d
SoskaVLG <57997351+SoskaVLG@users.noreply.github.com>
2020-12-21 21:48:13 +0300

    Add files via upload

---
+++ Factorial.cpp
@@ -0,0 +1,39 @@
+﻿#include <iostream>
+
+using namespace std ;
+
+void factorial(int x)
+{
+    int count = x ;
+
+    int i = 1 ;
+
+    int sum = 1 ;
+
+    while (count > 0) {
+        
+        sum *= i ;
+
+        i += 1 ;
+
+        count -= 1 ;
+    }
+
+    cout << "\a\nФакториал числа " << x << " равен : " << sum << "\n\n" ;
+
+    system("pause");
+}
+
+
+int main()
+{
+    setlocale(LC_ALL, "rus");
+
+    int a ;
+    cout << "Введите число : \a\n\n" << "> ";
+
+    cin >> a ;
+
+    factorial(a) ;
+}
+
